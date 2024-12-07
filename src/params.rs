4678d3a7d49975607c59d3333b7facb22a165efc
Onsraa <truongteddy306@icloud.com>
2024-12-07 16:03:48 +0100

    add simulation time param

--- src/params.rs
+++ src/params.rs
@@ -1,10 +1,12 @@
 //! Paramètres globaux modifiables
 
 pub const POPULATION_SIZE: usize = 50;
-pub const MAX_GENERATIONS: usize = 10_000;
+pub const MAX_GENERATIONS: usize = 1000;
 pub const ELITE_PERCENT: f64 = 0.10;
 pub const MUTATION_RATE: f64 = 0.01;
 pub const FITNESS_THRESHOLD: f64 = 1.9;
 
 pub const WIDTH: f32 = 500.0;
 pub const HEIGHT: f32 = 500.0;
+
+pub const SIMULATION_DURATION: f32 = 0.2;
